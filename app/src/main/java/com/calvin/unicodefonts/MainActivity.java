cd26d7c0556c41445c3c1e4d63bb2ce93caa72d8
Absolute-Zero172 <calvinjwasilevich@gmail.com>
2025-01-13 18:00:25 -0500

    added seekbars

--- app/src/main/java/com/calvin/unicodefonts/MainActivity.java
+++ app/src/main/java/com/calvin/unicodefonts/MainActivity.java
@@ -3,6 +3,8 @@ package com.calvin.unicodefonts;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.SeekBar;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,7 +15,7 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
 
     public void click(View v) {
-        Log.d("clicked", v.getResources().getResourceName(v.getId()));
+        Log.d("clicked button", v.getResources().getResourceName(v.getId()));
     }
 
     @Override
@@ -26,5 +28,50 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+
+        // initialize variance seekbar
+        SeekBar varianceSlider = (SeekBar)findViewById(R.id.varianceSlider);
+        ((TextView)findViewById(R.id.varianceView)).setText("Variance: 0");
+        varianceSlider.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        ((TextView)findViewById(R.id.varianceView)).setText("Variance: " + Integer.toString(progress));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
+
+        // initialize number seekbar
+        SeekBar numberSlider = (SeekBar)findViewById(R.id.numberSlider);
+        ((TextView)findViewById(R.id.numberView)).setText("Number: 1");
+        numberSlider.setOnSeekBarChangeListener(
+                new SeekBar.OnSeekBarChangeListener() {
+                    @Override
+                    public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                        ((TextView)findViewById(R.id.numberView)).setText("Number: " + Integer.toString(progress));
+                    }
+
+                    @Override
+                    public void onStartTrackingTouch(SeekBar seekBar) {
+
+                    }
+
+                    @Override
+                    public void onStopTrackingTouch(SeekBar seekBar) {
+
+                    }
+                }
+        );
     }
 }
